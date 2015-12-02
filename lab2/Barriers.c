47291e174c173a8e30f875dda999d505ccf04d02
Adam Burdett <burdettadam@gmail.com>
2015-12-02 10:03:03 -0700

    with barriers

---
+++ lab2/Barriers.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#ifdef __PTHREADS__
+#include <pthread.h>
+
+#ifdef defined(__KLOGB__)
+#define MAX_THREADS 32
+
+typedef struct barrier_node {
+        pthread_mutex_t count_lock;
+        pthread_cond_t ok_to_proceed_up;
+        pthread_cond_t ok_to_proceed_down;
+        int count;
+} mylib_barrier_t_internal;
+
+typedef struct barrier_node mylob_logbarrier_t[MAX_THREADS];
+int number_in_barrier = 0;
+pthread_mutex_t logbarrier_count_lock;
+
+void mylib_logbarrier (mylob_logbarrier_t b, int num_threads, int thread_id);
+void mylib_init_barrier(mylob_logbarrier_t b);
+
+mylob_logbarrier_t barr;
+#else
+typedef struct {
+        pthread_mutex_t count_lock;
+        pthread_cond_t ok_to_proceed;
+        int count;
+} mylib_barrier_t;
+
+void mylib_barrier (mylib_barrier_t *b, int num_threads);
+void mylib_init_barrier(mylib_barrier_t *b);
+
+mylib_barrier_t barr;
+#endif
+
+main()
+{
+#ifdef defined(__KLOGB__)
+	mylib_init_barrier(barr);
+#else
+	mylib_init_barrier(&barr);
+#endif
+
+#ifdef __PTHREADS__
+		qbusy_barrier(&barr, iproc, nproc);
+#ifdef __KLOGB__
+		mylib_logbarrier(barr, nproc, iproc);
+#else
+		mylib_barrier(&barr, nproc);
+#endif
+}
+#ifdef __KLOGB__
+
+void mylib_init_barrier(mylob_logbarrier_t b)
+{
+        int i;
+        for (i = 0; i < MAX_THREADS; i++) {
+                b[i].count = 0;
+                pthread_mutex_init(&(b[i].count_lock), NULL);
+                pthread_cond_init(&(b[i].ok_to_proceed_up), NULL);
+                pthread_cond_init(&(b[i].ok_to_proceed_down), NULL);
+        }
+		pthread_mutex_init(&logbarrier_count_lock, NULL);
+}
+
+void mylib_logbarrier (mylob_logbarrier_t b, int num_threads, int thread_id)
+{
+        int i, q, base, index;
+		float *tmp;
+        i = 2;
+        base = 0;
+
+		if (nproc == 1)
+			return;
+
+		pthread_mutex_lock(&logbarrier_count_lock);
+		number_in_barrier++;
+		if (number_in_barrier == nproc)
+		{
+				/* I am the last one in */
+				/* swap the new value pointer with the old value pointer */
+				tmp = oplate;
+				oplate = iplate;
+				iplate = tmp;
+				/*
+				fprintf(stderr,"%d: swapping pointers\n", thread_id);
+				*/
+
+				/* set the keepgoing flag and let everybody go */
+				keepgoing = 0;
+				for (q = 0; q < nproc; q++)
+					keepgoing += lkeepgoing[q];	
+		}
+		pthread_mutex_unlock(&logbarrier_count_lock);
+
+        do {
+                index = base + thread_id / i;
+                if (thread_id % i == 0) {
+                        pthread_mutex_lock(&(b[index].count_lock));
+                        b[index].count ++;
+                        while (b[index].count < 2)
+                              pthread_cond_wait(&(b[index].ok_to_proceed_up),
+                                        &(b[index].count_lock));
+                        pthread_mutex_unlock(&(b[index].count_lock));
+                }
+                else {
+                        pthread_mutex_lock(&(b[index].count_lock));
+                        b[index].count ++;
+                        if (b[index].count == 2)
+                           pthread_cond_signal(&(b[index].ok_to_proceed_up));
+/*
+			while (b[index].count != 0)
+*/
+			while (
+                               pthread_cond_wait(&(b[index].ok_to_proceed_down),
+                                    &(b[index].count_lock)) != 0);
+			pthread_mutex_unlock(&(b[index].count_lock));
+			break;
+                }
+                base = base + num_threads/i;
+                i = i * 2; 
+        } while (i <= num_threads);
+
+		i = i / 2;
+
+        for (; i > 1; i = i / 2)
+        {
+		base = base - num_threads/i;
+                index = base + thread_id / i;
+                pthread_mutex_lock(&(b[index].count_lock));
+                b[index].count = 0;
+                pthread_cond_signal(&(b[index].ok_to_proceed_down));
+                pthread_mutex_unlock(&(b[index].count_lock));
+        }
+		pthread_mutex_lock(&logbarrier_count_lock);
+		number_in_barrier--;
+		pthread_mutex_unlock(&logbarrier_count_lock);
+}
+
+
+#else
+
+void mylib_init_barrier(mylib_barrier_t *b)
+{
+        b -> count = 0;
+        pthread_mutex_init(&(b -> count_lock), NULL);
+        pthread_cond_init(&(b -> ok_to_proceed), NULL);
+}
+
+void mylib_barrier (mylib_barrier_t *b, int num_threads)
+{
+		int i;
+		float *tmp;
+		if (num_threads == 1)
+			return;
+        pthread_mutex_lock(&(b -> count_lock));
+        b -> count ++;
+        if (b -> count == num_threads) 
+		{
+				/* I am the last one in */
+				/* swap the new value pointer with the old value pointer */
+				tmp = oplate;
+				oplate = iplate;
+				iplate = tmp;
+
+				/* set the keepgoing flag and let everybody go */
+				keepgoing = 0;
+				for (i = 0; i < nproc; i++)
+					keepgoing += lkeepgoing[i];
+                b -> count = 0;
+                pthread_cond_broadcast(&(b -> ok_to_proceed));
+        }
+        else
+                while (pthread_cond_wait(&(b -> ok_to_proceed), &(b -> count_lock)) != 0);
+        pthread_mutex_unlock(&(b -> count_lock));
+}
+
+#endif
--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
 #include <pthread.h>
 pthread_mutex_t mutexA;
 //#define tableSize 16384
@@ -15,6 +14,9 @@ pthread_mutex_t mutexA;
 //#define tableSize 1024
 //#define tableSize 512
 #define EPSILON  0.1
+#define barrier_type "l"// linear
+//#define barrier_type "k"// log
+//#define barrier_type "p"// libary
 pthread_barrier_t   barrier; // barrier synchronization object
 float** currentMatrix;
 float** lastMatrix;
@@ -30,7 +32,140 @@ typedef struct barrier_node {
         int count;
 } mylib_barrier_t_internal;
 
+typedef struct barrier_node mylob_logbarrier_t[MAX_THREADS];
+int number_in_barrier = 0;
+pthread_mutex_t logbarrier_count_lock;
+mylob_logbarrier_t barr;
+
+struct LinearBarrier {
+    pthread_mutex_t count_lock;
+    pthread_cond_t ok_to_proceed;
+    int count;
+};
+
+
+LinearBarrier linearBarrier;
+
 using namespace std;
+void mylib_init_barrier(mylob_logbarrier_t b)
+{
+        int i;
+        for (i = 0; i < MAX_THREADS; i++) {
+                b[i].count = 0;
+                pthread_mutex_init(&(b[i].count_lock), NULL);
+                pthread_cond_init(&(b[i].ok_to_proceed_up), NULL);
+                pthread_cond_init(&(b[i].ok_to_proceed_down), NULL);
+        }
+    pthread_mutex_init(&logbarrier_count_lock, NULL);
+}
+void mylib_logbarrier (mylob_logbarrier_t b, int num_threads, int thread_id)
+{
+        int i, q, base, index;
+    float *tmp;
+        i = 2;
+        base = 0;
+
+    if (nproc == 1)
+      return;
+
+    pthread_mutex_lock(&logbarrier_count_lock);
+    number_in_barrier++;
+  /* I swap else where, with a mutex
+      if (number_in_barrier == nproc)
+    {
+        // I am the last one in 
+        // swap the new value pointer with the old value pointer 
+        tmp = oplate;
+        oplate = iplate;
+        iplate = tmp;
+        //fprintf(stderr,"%d: swapping pointers\n", thread_id);
+
+        // set the keepgoing flag and let everybody go 
+        keepgoing = 0;
+        for (q = 0; q < nproc; q++)
+          keepgoing += lkeepgoing[q]; 
+    }
+  */
+      pthread_mutex_unlock(&logbarrier_count_lock);
+
+        do {
+                index = base + thread_id / i;
+                if (thread_id % i == 0) {
+                        pthread_mutex_lock(&(b[index].count_lock));
+                        b[index].count ++;
+                        while (b[index].count < 2)
+                            pthread_cond_wait(&(b[index].ok_to_proceed_up), &(b[index].count_lock));
+                        pthread_mutex_unlock(&(b[index].count_lock));
+                }
+                else {
+                        pthread_mutex_lock(&(b[index].count_lock));
+                        b[index].count ++;
+                        if (b[index].count == 2)
+                           pthread_cond_signal(&(b[index].ok_to_proceed_up));
+/*
+      while (b[index].count != 0)
+*/
+      while (
+                               pthread_cond_wait(&(b[index].ok_to_proceed_down),
+                                    &(b[index].count_lock)) != 0);
+      pthread_mutex_unlock(&(b[index].count_lock));
+      break;
+                }
+                base = base + num_threads/i;
+                i = i * 2; 
+        } while (i <= num_threads);
+
+    i = i / 2;
+
+        for (; i > 1; i = i / 2)
+        {
+    base = base - num_threads/i;
+                index = base + thread_id / i;
+                pthread_mutex_lock(&(b[index].count_lock));
+                b[index].count = 0;
+                pthread_cond_signal(&(b[index].ok_to_proceed_down));
+                pthread_mutex_unlock(&(b[index].count_lock));
+        }
+    pthread_mutex_lock(&logbarrier_count_lock);
+    number_in_barrier--;
+    pthread_mutex_unlock(&logbarrier_count_lock);
+}
+void myLinearBarrierInit(mylib_barrier_t *b)
+{
+        b -> count = 0;
+        pthread_mutex_init(&(b -> count_lock), NULL);
+        pthread_cond_init(&(b -> ok_to_proceed), NULL);
+}
+void myLinearBarrier (mylib_barrier_t *b, int num_threads)
+{
+    if (num_threads == 1)
+      return;
+        pthread_mutex_lock(&(b -> count_lock));
+        b -> count ++;
+        if (b -> count == num_threads) 
+    {
+            b -> count = 0;
+            pthread_cond_broadcast(&(b -> ok_to_proceed));
+        }
+        else
+                while (pthread_cond_wait(&(b -> ok_to_proceed), &(b -> count_lock)) != 0);
+        pthread_mutex_unlock(&(b -> count_lock));
+}
+void barrier(id){
+  if (barrier_type == 'l'){
+    myLinearBarrier(&linearBarrier);
+  }
+  else if (barrier_type == 'k'){
+    mylib_logbarrier(barr, num_threads, id);
+  }
+  else //(barrier_type == 'p')
+  {
+    pthread_barrier_wait(&barrier);
+
+  }
+
+}
+
 double When()
 {
     struct timeval tp;
@@ -41,6 +176,16 @@ float fabs(float f)
 {
     return (f > 0.0)? f : -f ;
 }
+
+void destroy() {
+  for( int i =0; i < tableSize; i++) {
+    free(currentMatrix[i]);
+    free(lastMatrix[i]);
+  }
+  free(currentMatrix);
+  free(lastMatrix);
+}
+
 void fillplate(float** current,float** old,){
    for (int row = 0 ; row < tableSize; row++ ) {
         for (int col = 0 ; col < tableSize; col++) {
@@ -150,20 +295,22 @@ void *HotPlate(void *threadid)
   
    for (int i = 0; i < iterations; ++i)
    {
-      pthread_barrier_wait (&barrier);
+      barrier(tid);
       update(lastMatrix, currentMatrix, start, stop);
-      pthread_barrier_wait (&barrier);
+      barrier(tid);
       //check
       graterThanCount(tid, start, stop, currentMatrix, lastMatrix);
-      pthread_barrier_wait (&barrier);
+      barrier(tid);
       /* Swap the pointers */
       if (tid == 0){
+        pthread_mutex_lock (&mutexA);
         tmp = currentMatrix;
         currentMatrix = lastMatrix;
         lastMatrix = tmp;
+        pthread_mutex_unlock(&mutexA);
       }
    }
-  pthread_barrier_wait (&barrier);
+  barrier(tid);
    //cout << "Hello World! Thread ID, " << tid << endl;
    pthread_exit(NULL);
 }
@@ -199,6 +346,7 @@ int main()
     fifCount += count[counter];
   }
   end = When();
+  destroy();
   printf("Total iterations: %d with %d > EPSILON\n", iterations, fifCount);
   printf("Total time: %d\n", (end - start));
   pthread_exit(NULL);
