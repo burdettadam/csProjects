d531c0ca8b3c1e8bab7a29996206b38bd309f28c
Adam Burdett <burdettadam@gmail.com>
2015-12-02 14:16:48 -0700

    sh correct

--- lab2/lab2.sh
+++ lab2/lab2.sh
@@ -1,4 +1,6 @@
-BATCH --time=02:00:00   # walltime
+#!/bin/bash
+
+#SBATCH --time=02:00:00   # walltime
 #SBATCH --ntasks=24   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=1024M   # memory per CPU core
