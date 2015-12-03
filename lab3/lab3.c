f107d395aa9acb5f531c9c664e31194252d2f6dc
Adam Burdett <burdettadam@gmail.com>
2015-12-03 14:03:27 -0700

    20480

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -12,7 +12,7 @@
 #include <mpi.h>
 #include <math.h>
 
-#define VECSIZE 5120 // numbers on each node?
+#define VECSIZE 20480 // numbers on each node?
 #define ITERATIONS 10000 //
 struct node{
     double val;
