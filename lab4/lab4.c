98bb8a7344663dbf42293f2eae818858b44a26e2
Adam Burdett <burdettadam@gmail.com>
2015-12-03 20:50:28 -0700

    keep willy

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -167,12 +167,12 @@ int main(int argc, char *argv[])
             reallydone = 1 ;
         }
     }
-    for( int i =0; i < SIZE; i++) {
-        free(currentMatrix[i]);
-        free(lastMatrix[i]);
-      }
-      free(currentMatrix);
-      free(lastMatrix);
+//    for( int i =0; i < SIZE; i++) {
+ //       free(currentMatrix[i]);
+//        free(lastMatrix[i]);
+//      }
+//      free(currentMatrix);
+//      free(lastMatrix);
 
     /* print out the number of iterations to relax */
     if (iproc == 0) {
