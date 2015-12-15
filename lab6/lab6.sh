dc4e368f4708dddc94794ad70e55df8b81531b98
Adam Burdett <burdettadam@gmail.com>
2015-12-14 22:20:29 -0700

    correct compile command

--- lab6/lab6.sh
+++ lab6/lab6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --time=00:10:00   # walltime
-#SBATCH --nodes=256   # number of nodes
+#SBATCH --time=00:7:00   # walltime
+#SBATCH --nodes=24   # number of nodes
 #SBATCH --mem-per-cpu=2024M   # memory per CPU core
 #SBATCH -J "lab6"   # job name
 #SBATCH --mail-user=burdettadam@gmail.com   # email address
@@ -17,7 +17,7 @@ export PBS_QUEUE=batch
 
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-mpic++ -o3 lab6 mendels2Mpi.cpp  -O3
+mpic++ -O3  mendelMpi.cpp -o lab6
 echo "2Th_lab6"
 mpirun -np 2 lab6
 echo "4Th_lab6"
@@ -28,11 +28,11 @@ echo "16Th_lab6"
 mpirun -np 16 lab6
 echo "24Th_lab6"
 mpirun -np 24 lab6
-echo "32Th_lab6"
-mpirun -np 32 lab6
-echo "64Th_lab6"
-mpirun -np 64 lab6
-echo "128Th_lab6"
-mpirun -np 128 lab6
-echo "256Th_lab6"
-mpirun -np 256 lab6
+#echo "32Th_lab6"
+#mpirun -np 32 lab6
+#echo "64Th_lab6"
+#mpirun -np 64 lab6
+#echo "128Th_lab6"
+#mpirun -np 128 lab6
+#echo "256Th_lab6"
+#mpirun -np 256 lab6
