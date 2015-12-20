261c44a3d01b9f4420f2f7a0d9ba1311c5def242
Adam Burdett <burdettadam@gmail.com>
2015-12-20 16:36:22 -0700

    more memory

--- lab7/lab7.sh
+++ lab7/lab7.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=0:10:00   # walltime
+#SBATCH --time=0:05:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=1024M   # memory per CPU core
+#SBATCH --mem-per-cpu=16G   # memory per CPU core
 #SBATCH -J "test1"   # job name
 
 module load cuda/7.0.28
